6cecc3e8d4c22ed8a77bafdb3fabcc6a9ea82257
Malek Brachemi <brachemi.malek92@gmail.com>
2025-10-25 13:44:11 -0400

    added uncrustify instructions, but didnt work

--- force-app/platform cache/lwc/platformCacheLwc/platformCacheLwc.js
+++ force-app/platform cache/lwc/platformCacheLwc/platformCacheLwc.js
@@ -1,33 +1,29 @@
-import { LightningElement } from 'lwc';
-import storeAccountToCache from '@salesforce/apex/PartitionCacheController.storeAccountToCache'
-import getAccountBySOQL from '@salesforce/apex/PartitionCacheController.getAccountBySOQL'
-import getAccountFromCache from '@salesforce/apex/PartitionCacheController.getAccountByCachePartition'
+import { LightningElement } from "lwc";
+import storeAccountToCache from "@salesforce/apex/PartitionCacheController.storeAccountToCache";
+import getAccountBySOQL from "@salesforce/apex/PartitionCacheController.getAccountBySOQL";
+import getAccountFromCache from "@salesforce/apex/PartitionCacheController.getAccountByCachePartition";
 export default class PlatformCacheLwc extends LightningElement {
+  async connectedCallback() {
+    storeAccountToCache().then(() => {
+      console.log(" :::::::: Account Cached ::::::::");
+    });
+  }
 
-
-    async connectedCallback(){
-        storeAccountToCache().then(() =>{
-            console.log(" :::::::: Account Cached ::::::::");
-        })
-    }
-
-    async retrieveDataSoql(){
+  async retrieveDataSoql() {
     let startTime = Date.now();
-        getAccountBySOQL().then((data)=>{
-            console.log(" Account by SOQL : " + JSON.stringify(data))
-            let operationTime = Date.now() - startTime;
-        console.log("OPERATION TIME SOQL = " + operationTime);
-        })
-    }
+    getAccountBySOQL().then((data) => {
+      console.log(" Account by SOQL : " + JSON.stringify(data));
+      let operationTime = Date.now() - startTime;
+      console.log("OPERATION TIME SOQL = " + operationTime);
+    });
+  }
 
-    async retrieveDataCache(){
-        let startTime = Date.now();
-        getAccountFromCache().then((data)=>{
-            console.log(" Account by Cache : " + JSON.stringify(data))
-            let operationTime = Date.now() - startTime;
-            console.log("OPERATION TIME CACHE = " + operationTime);
-        })
-    }
-
-
+  async retrieveDataCache() {
+    let startTime = Date.now();
+    getAccountFromCache().then((data) => {
+      console.log(" Account by Cache : " + JSON.stringify(data));
+      let operationTime = Date.now() - startTime;
+      console.log("OPERATION TIME CACHE = " + operationTime);
+    });
+  }
 }
